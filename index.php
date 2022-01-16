f9b01b5205fe0fd2240f2686ac43f21d45dfa45c
Xyprime <90967932+Xyprime@users.noreply.github.com>
2022-01-16 12:31:09 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+
+
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>MultiStaion</title>
+</head>
+<body>
+    <h1>Multistation</h1>
+    <hr>
+    <form name="bt">
+        <input type="submit" value="Reload" />
+    </form>
+</body>
+</html>
